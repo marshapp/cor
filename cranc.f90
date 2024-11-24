6bc847722f7b53361f62713b61fc845a69a127b2
isaacbg25 <isaacbaldigarcia@gmail.com>
2024-11-24 10:45:17 +0100

    falta que els valors siguin raonables

--- cranc.f90
+++ cranc.f90
@@ -1,41 +1,101 @@
 Program cranc
 
    
-    integer :: N=101
-    integer :: i
-    real :: Az 
-    real :: At
-    integer :: h,g,j
-    integer :: c=490
-    real :: Tc = 36.5
-    real :: cv = 3686
-    real :: r = 1081
-    real :: k=0.56
-    real :: s=0.472
-    real :: v=40
-    real :: L=0.02
-    real :: p 
-    real :: a
-    real :: b
-    real :: A_z=0.49
-    real :: Tc_n
-    REAL, ALLOCATABLE :: T(:,:)
-    Az = 1.0/(N-1)
-    At = A_z*(Az**2)
-    p = (s*v**2)/(2*L**2)
-    a=k/(cv*r)
-    b=p/(r*cv)
-    Tc_n=(Tc*a)/(b*L)
-    c=int(0.025/At)
-    allocate(T(0:N-1,0:N-1))
+    integer,parameter :: N=101
+    real, parameter :: Az =1.0/(N-1), Tc = 36.5, cv = 3686, r = 1081, k=0.56, s=0.472, v=40,  L=0.02, A_z=0.49
+    real,parameter :: At= A_z*(Az**2)
+    integer,parameter :: c=int(0.025/At)
+    real,parameter :: p = (s*v**2)/(2*L**2)
+    real, parameter :: alpha = k/(cv*r)
+    real,parameter :: beta=p/(r*cv)
+    real,parameter :: Tc_n=(Tc*alpha)/(beta*L)
+    
+    integer, parameter :: len=1000
+    real :: A(n-1,n-1), b(n-1), x(n-1,0:c), x_1(n-1,0:c)
+    integer :: iter, i,j,t,h,g
+    real :: sum, error
+    real, parameter :: tolerancia=1.0e-9
 
-    do i = 1, N-1
-         T(i,i)=-A_z/2
-         T(i,i+1)=A_z/2
-         T(i,i+2)=-A_z/2
+
+    
+    do i=1,n-1 !matriu a  tot a 0, matriu x tot a 0 (després: menys a t=0  que val Tc)
+        do j=1,n-1
+            A(i,j)= 0
+        end do
+    end do
+    do i=1,n-1 ! x tot a 0 (després: menys a t=0  que val Tc)
+        do j=1,c
+            x(i,j)= 0
+        end do
     end do
 
-    write(*, *) T(5,:)
+    A(1,1)= A_z+1 
+    A(1,2)= -A_z/2
+    A(N-1,N-2)= -A_z/2
+    A(N-1,N-1)=A_z+1
+
+    do i = 2, N-2
+        A(i,i-1)=-A_z/2
+        A(i,i)=(A_z+1)
+        A(i,i+1)=-A_z/2
+    end do
+
+    do h = 1,N-1 !a temps 0 la temp és tc a tot l'espai
+        x(h,0)= Tc_n !ci
+        x_1(h,0)=Tc_n
+    end do
+
+    
+
+    do t= 0, c-1 !c-1 perque et donara x a tems t+1
+        do j = 2,N-2
+            B(1)=(A_z/2)*x(2,t)-(A_z-1)*x(1,t)+(A_z/2)*Tc_n+At+(A_z/2)*Tc_n
+            B(N-1)=(A_z/2)*Tc_n-(A_z-1)*x(n-1,t)+(A_z/2)*x(n-2,t)+At+(A_z/2)*Tc_n
+            B(j)=(A_z/2)*x(j+1,t)-(A_z-1)*x(j,t)+(A_z/2)*x(j-1,t)+At
+        end do
+        !print *, "ja estic resolent el teu sistema"
+    
+        do iter = 1, len
+        
+            x_1(:,t+1)= x(:,t+1) !la x de l'anterior iteració ara és x_1
+            do i = 1, N-1 !suma per cada x(i) 
+                sum =0
+                do j = 1, N-1
+                    if (j/=i) then
+                        sum = sum + A(i,j)*x_1(j,t+1)
+                    end if
+                end do 
+                x(i,t+1)=(b(i)-sum)/A(i,i) !genera la nova x
+            end do
+            error = maxval(abs(x(:,t+1) - x_1(:,t+1)))
+
+            if (error < tolerancia) then
+                !print*, "ha convergit en", iter, "iteracions"
+                exit
+            end if
+            if (iter==len) then
+                print*, "no ha convergit ): sap greu tant jove"
+                stop 
+            end if
+            
+        end do
+    end do   
+        
+
+        
+
+
+    open(unit=10, file="cranc_data.txt", status="replace")   
+    do i = 1, n-1
+           
+        write(10, '(F10.6, 1X, F10.6)') Az+(i*Az), x(i,c)*r*cv/p
+    end do
+
+
+    close(10)
+
+
+    write(*, *)  x(:,c)*((beta*l)/alpha) 
 end program cranc
 
 
