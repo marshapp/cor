0e8b0511793174950663962f68b1e94902f57eed
isaacbg25 <isaacbaldigarcia@gmail.com>
2024-11-25 16:30:35 +0100

    ara sí que sí

--- cranc.f90
+++ cranc.f90
@@ -1,8 +1,8 @@
 Program cranc
 
     implicit none(type, external)
-    integer,parameter :: N=101
-    real, parameter :: Az =1.0/(N-1), Tc = 36.5, cv = 3686, r = 1081, k=0.56, s=0.472, v=40,  L=0.02, A_z=0.25
+    integer,parameter :: N=101 !al començar a n=0 hi haura n+1 punts
+    real, parameter :: Az =1.0/(N), Tc = 36.5, cv = 3686, r = 1081, k=0.56, s=0.472, v=40,  L=0.02, A_z=0.25
     real,parameter :: At= A_z*(Az**2)
     integer,parameter :: c=int(0.025/At)
     real,parameter :: p = (s*v**2)/(2*L**2)
@@ -11,7 +11,7 @@ Program cranc
     real,parameter :: Tc_n=(Tc*alpha)/(beta*L**2)
     
     integer, parameter :: len=1000
-    real :: A(n-1,n-1), b(n-1), x(n-1,0:c), x_1(n-1,0:c)
+    real :: A(n-1,n-1), b(n-1), x(0:n,0:c), x_1(0:n,0:c)
     integer :: iter, i,j,t,h,g
     real :: sum, error
     real, parameter :: tolerancia=1.0e-9
@@ -40,12 +40,17 @@ Program cranc
         A(i,i+1)=-A_z/2
     end do
 
-    do h = 1,N-1 !a temps 0 la temp és tc a tot l'espai
+    do h = 0,N !a temps 0 la temp és tc a tot l'espai
         x(h,0)= Tc_n !ci
         x_1(h,0)=Tc_n
     end do
 
-    
+    do h=0,c
+        x(0,h)=Tc_n
+        x(N,h)=Tc_n
+        x_1(0,h)=Tc_n
+        x_1(N,h)=Tc_n
+    end do
 
     do t= 0, c-1 !c-1 perque et donara x a tems t+1
         do j = 2,N-2
@@ -86,18 +91,18 @@ Program cranc
 
 
     open(unit=10, file="cranc_data.txt", status="replace")   
-    do i = 1, n-1
+    do i = 0, n
            
-        write(10, '(F10.6, 1X, F10.6)') Az*l+(i*Az)*l, x(i,c)*((beta*(l**2))/alpha) 
+        write(10, '(F10.6, 1X, F10.6)') (i*Az)*l, x(i,c)*((beta*(l**2))/alpha) 
     end do
 
 
     close(10)
 
     open(unit=11, file="camp.txt", status="replace")   
-    do i = 1, n-1
-        do j=1,c
-           write(11, '(F10.6, 1X, F10.6, 1X, F10.6)') Az*l+(i*Az)*l, j*At, x(i,j)*((beta*(l**2))/alpha) 
+    do i = 0, n
+        do j=0,c
+           write(11, '(F10.6, 1X, F10.6, 1X, F10.6)') (i*Az)*l, j*At, x(i,j)*((beta*(l**2))/alpha) 
         end do
         write(11,*)
     end do
