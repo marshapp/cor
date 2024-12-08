a4805207391feaf177da562fbb290fb533856a40
marshapp <139222511+marshapp@users.noreply.github.com>
2024-12-08 22:36:58 +0100

    Update explicit.f90

--- definitiu/euler_explicit/explicit.f90
+++ definitiu/euler_explicit/explicit.f90
@@ -1,16 +1,18 @@
-program euler_explicit
+program explicit
 
     implicit none(type, external) 
     integer :: i, j, m, u, w, N_t
     integer, parameter :: N_z = 101
-    real :: delta_t, alpha, lambda, P_ext, beta, gamma(3), R(0:N_z-1,3), delta_z
+    real :: dz, dt, alpha, lambda, P_ext, beta, gamma(3), R(0:N_z-1,3)
     real, parameter :: L = 0.02, V = 40, c_v = 3686, rho = 1081, k = 0.56, sigma = 0.472, T_c=36.5, t_a=0.025
     real, allocatable :: T(:,:) 
+    
+    dz = 1/(real(N_z)-1) !Definim l'interval espacial
 
-    delta_z = 1/(real(N_z)-1)
-
+    !Definim els diferents valors de gamma o mallats que utilitzarem:
     gamma=[0.51, 0.49, 0.25]
-
+    
+    !Definim diferents constants del problema:
     alpha = k/(c_v*rho)
 
     P_ext = ((V**2)*sigma)/(2*(L**2))
@@ -20,13 +22,14 @@ program euler_explicit
     beta = T_c*((alpha)/(lambda*(L**2)))
 
 
-    do m=1, 3
+    do m=1, 3 !Fem un bucle per tenir en compte els 3 valors de gamma
+        
+        dt = gamma(m)*(dz**2) !Definim el dt associat a aquest valor de gamma
 
-        delta_t = gamma(m)*(delta_z**2)
+        N_t=int(t_a/dt) !Definim el nombre de passos temporals que depèn de dt i, per tant, també de gamma
 
-        N_t=int(t_a/delta_t)
-
-        allocate(T(0:N_z-1, 0:N_t))
+        allocate(T(0:N_z-1, 0:N_t)) !Definim les dimensions de la matriu que emmagatzema 
+        !els valors de la temperatura a cada temps i posició per aquest valor de gamma
 
         !Apliquem la condició inicial:
         do j = 0, N_z-1
@@ -39,10 +42,10 @@ program euler_explicit
             T(N_z-1,i) = beta
         end  do
 
-        !Trobem els demés punts utilitzant la iteració del mètode:
-        do i = 0, N_t-1 !Excloem i=N_t per evitar sortir-nos de la matriu.
-            do j = 1, N_z-2 !Excloem j=0 i j=N_x-1 per evitar sortir-nos de la matriu.
-                T(j,i+1)=(T(j+1,i)-2*T(j,i)+T(j-1,i))*gamma(m)+delta_t+T(j,i)
+        !Trobem els demés punts utilitzant l'equació corresponent pel temps i+1 i la posició j:
+        do i = 0, N_t-1 !Excloem i=N_t per evitar sortir-nos de la matriu
+            do j = 1, N_z-2 !Excloem j=0 i j=N_x-1 per evitar sortir-nos de la matriu
+                T(j,i+1)=(T(j+1,i)-2*T(j,i)+T(j-1,i))*gamma(m)+dt+T(j,i)
             end do
         end do
 
@@ -51,18 +54,17 @@ program euler_explicit
             R(j,m) = T(j, N_t)*((lambda*(L**2))/alpha)
         end do
 
-
-
-        deallocate(T)
+        deallocate(T) !Reiniciem la matriu per a emmagatzemar els valors de la temperatura pel següent valor de gamma
  
     end do    
+
 
     open(newunit=u, file='explicit.dat', status='unknown', action='write')
 
     !Escribim en un fitxer els punts espacials sense normalitzar i els resultats corresponents a cada gamma, 
     !en 4 columnes diferents per facilitar el seu graficat:
     do j = 0, N_z-1
-        write(u, *) (j*delta_z)*L, R(j,1), R(j,2), R(j,3), f(200, j*delta_z)
+        write(u, *) (j*dz)*L, R(j,1), R(j,2), R(j,3), f(200, j*dz)
     end do
 
     close(u)
@@ -70,34 +72,35 @@ program euler_explicit
 
     open(NEWUNIT=w, file='error_explicit.dat', status='unknown', action='write')
 
-    !Escribim en un fitxer els punts espacials sense normalitzar i els errors absoluts corresponents a gamma=0.49
-    !i a gamma=0.25 en 3 columnes diferents per facilitar el seu graficat:
+    !Escribim en un fitxer els punts espacials sense normalitzar i els errors absoluts corresponents a gamma=0,49
+    !i a gamma=0,25 en 3 columnes diferents per facilitar el seu graficat:
     do j = 0, N_z-1
-        write(w, *) (j*delta_z)*L, abs(R(j,2)-f(200, j*delta_z)), abs(R(j,3)-f(200, j*delta_z))
+        write(w, *) (j*dz)*L, abs(R(j,2)-f(200, j*dz)), abs(R(j,3)-f(200, j*dz))
     end do
 
     close(w)
 
-    contains
-
+    contains 
+    !Aquí definim la funció corresponent a la solució analítica pel temps normalitzat t_a
+    !que pren per arguments la posició normalitzada i el nombre de termes del sumatori que apareix a la seva expressió    
         real function f(N, z_norm)
 
             implicit none(type, external)
             integer, intent(in) :: N
             real, intent(in) :: z_norm
-            real, parameter :: t_norm=0.025, pi=2*acos(0.0)
+            real, parameter :: pi=2*acos(0.0)
             integer :: k
             real :: sumatori, f_norm
 
             sumatori=0 
 
             do k = 1, N
-                sumatori = sumatori + ((1-exp(-((2*k-1)**2)*(pi**2)*t_norm))/((2*k-1)**3))*sin((2*k-1)*pi*z_norm)
+                sumatori = sumatori + ((1-exp(-((2*k-1)**2)*(pi**2)*t_a))/((2*k-1)**3))*sin((2*k-1)*pi*z_norm)
             end do
 
             f_norm=beta+(4/(pi**3))*sumatori
-            f=(f_norm*lambda*(L**2))/alpha
+            f=(f_norm*lambda*(L**2))/alpha !Desnormalitzem el resultat de sortida
 
         end function
 
-end program euler_explicit
+end program explicit
